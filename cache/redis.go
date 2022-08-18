5917b328f4be5fc84172ae1126cf54e362fd17ad
marcos-dev88 <mv.piesske@gmail.com>
2022-08-17 23:09:18 -0300

    updating cache package
    Signed-off-by: marcos-dev88 <mv.piesske@gmail.com>

---
+++ cache/redis.go
@@ -0,0 +1,37 @@
+package cache
+
+import (
+	"context"
+	"time"
+
+	redis "github.com/go-redis/redis/v8"
+)
+
+type Cache interface {
+	Set(key string, data interface{}, ttl time.Duration) error
+	Get(key string) ([]byte, error)
+}
+
+func NewClient(addr, pass string, db int) *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: pass,
+		DB:       db,
+	})
+}
+
+func New(client *redis.Client, ctx context.Context) Cache {
+	return &cache{Client: client, Ctx: ctx}
+}
+
+func (c cache) Set(key string, data interface{}, ttl time.Duration) error {
+	client := c.Client
+	defer client.Close()
+	return client.Set(c.Ctx, key, data, ttl).Err()
+}
+
+func (c cache) Get(key string) ([]byte, error) {
+	client := c.Client
+	defer client.Close()
+	return client.Get(c.Ctx, key).Bytes()
+}
